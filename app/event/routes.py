5b5f7b9e3d79b82cc42fe44944229188362de8f1
joekariuki3 <joekariuki3@gmail.com>
2025-05-26 22:38:05 +0300

    Refactor event routes to utilize EventService class
    Replaced direct function calls with EventService methods for better encapsulation and maintainability. This reduces redundancy and centralizes event-related logic within the service layer, improving code readability and organization.

--- app/event/routes.py
+++ app/event/routes.py
@@ -1,11 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request, session
 from flask_login import current_user, login_required
 from . import bp
-from datetime import datetime
-from app.models.event import Event
 from .forms import AddEventForm
-from app.extensions import db
-from .services import create_event, get_upcoming_events, get_past_events, delete_an_event, get_event, event_belongs_to_current_user, update_event
+from .services import EventService
 
 @bp.route('/event/', methods=['POST', 'GET'])
 @login_required
@@ -17,10 +14,12 @@ def add_event():
 
     form = AddEventForm()
     if form.validate_on_submit():
-        data, status = create_event(
+        event_service = EventService()
+
+        data, status = event_service.create_event(
             event_date=form.date.data,
             event_name=form.name.data,
-            family_id=request.form.get('family'),
+            family_id=int(request.form.get('family')),
             event_location=form.location.data,
             event_description=form.description.data
             )
@@ -32,24 +31,22 @@ def add_event():
             return render_template('add_event.html', title='Add Event', form=form)
 
         flash(message, category)
-        return(redirect(url_for('event.get_events', family_id=event.family_id)))
+        return redirect(url_for('event.get_events', family_id=event.family_id))
     return render_template('add_event.html', title='Add Event', form=form, families=families)
 
 @bp.route('/event/<family_id>', methods=['POST', 'GET'])
 @login_required
 def get_events(family_id):
-    data, status = get_upcoming_events(family_id)
+    event_service = EventService()
+
+    data, status = event_service.get_upcoming_events(family_id)
     upcoming_events, message, category = data.get('data', []), data.get('message'), data.get('category')
     if status != 200:
         flash(message, category)
-    elif not upcoming_events:
-        flash(message, category)
 
-    data, status = get_past_events(family_id)
+    data, status = event_service.get_past_events(family_id)
     past_events, message, category = data.get('data', []), data.get('message'), data.get('category')
     if status != 200:
-        flash(message, category)
-    elif not past_events:
         flash(message, category)
 
     return render_template('events.html', upcomingEvents=upcoming_events, pastEvents=past_events)
@@ -57,39 +54,37 @@ def get_events(family_id):
 @bp.route('/delete/event/<event_id>')
 @login_required
 def delete_event(event_id):
-    data, status = delete_an_event(event_id)
+    event_service = EventService()
+
+    data, status = event_service.delete_an_event(event_id)
 
     event, message, category = data.get('data'), data.get('message'), data.get('category')
 
     if status != 200:
         flash(message, category)
-    elif not event:
-        flash(message, category)
-    else:
-        flash(message, category)
 
+    flash(message, category)
     return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
 
 @bp.route('/edit/event/<event_id>', methods=['GET', 'POST'])
 @login_required
 def edit_event(event_id):
-    data, status = get_event(event_id)
+    event_service = EventService()
+
+    data, status = event_service.get_event(event_id)
     event, message, category = data.get('data'), data.get('message'), data.get('category')
     if status != 200:
         flash(message, category)
         return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
-    elif not event:
-        flash(message, category)
-        return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
 
-    if not event_belongs_to_current_user(event, current_user):
+    if not event_service.event_belongs_to_current_user(event, current_user):
         flash('You are not authorized to edit this event', 'danger')
         return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
 
     form = AddEventForm()
 
     if form.validate_on_submit():
-        data, status = update_event(
+        data, status = event_service.update_event(
             event = event,
             event_date=form.date.data,
             event_name=form.name.data,
