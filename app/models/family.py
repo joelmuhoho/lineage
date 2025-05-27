bb5944d1318f3c1dd66bed9715915309a8bff0b9
joekariuki3 <joekariuki3@gmail.com>
2025-05-27 14:39:48 +0300

    Refactor Family model to enhance type safety and documentation
    Updated the Family model by adding type hints and improving inline documentation. These changes provide better clarity, ensure compatibility with related entities, and enhance the maintainability of the codebase.

--- app/models/family.py
+++ app/models/family.py
@@ -1,14 +1,40 @@
 from . import db
+from .user import User
+from .member import Member
+from .event import  Event
+from .link import  Link
+from typing import List
+from sqlalchemy.orm import Mapped
 
 class Family(db.Model):
-    """Class representing a family in the application."""
+    """
+    Represents a family entity in the application.
+
+    This class models a family in the database, including its relationships to users,
+    events, members, and links. It defines the family structure and facilitates interactions
+    with other entities in the system. Each family has a unique ID, a name, and optionally
+    associates with a user. The class supports cascading delete operations for related entities.
+
+    Attributes:
+        family_id: The unique identifier for the family.
+        name: The name of the family.
+        user_id: An optional foreign key referencing the user associated with the family.
+        users: The relationship to the User model, representing users associated with the family.
+        events: The relationship to the Event model, allowing cascading delete for family-related events.
+        members: The relationship to the Member model, enabling cascading delete orphans for members of the family.
+        links: The relationship to the Link model, enabling cascading delete orphans for links associated with the family.
+    """
+    MAX_STRING_LENGTH = 50
     __tablename__ = 'families'
 
-    family_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
+    family_id: Mapped[int] = db.Column(db.Integer, primary_key=True)
+    name: Mapped[str] = db.Column(db.String(MAX_STRING_LENGTH), nullable=False)
+
+    # Foreign key
+    user_id: Mapped[int] = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
+
     # Relationships
-    users = db.relationship('User', back_populates='families')
-    events = db.relationship('Event', cascade="all,delete", back_populates='family')
-    members = db.relationship('Member', cascade="all,delete-orphan", back_populates='family')
-    links = db.relationship('Link', cascade="all,delete-orphan", back_populates='family')
+    users: Mapped[List[User]] = db.relationship('User', back_populates='families')
+    events: Mapped[List[Event]] = db.relationship('Event', cascade="all,delete", back_populates='family')
+    members: Mapped[List[Member]] = db.relationship('Member', cascade="all,delete-orphan", back_populates='family')
+    links: Mapped[List[Link]] = db.relationship('Link', cascade="all,delete-orphan", back_populates='family')
