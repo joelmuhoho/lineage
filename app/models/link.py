9de8ccec87f6648d4c43ac45e1d9c0c7cd182005
joekariuki3 <joekariuki3@gmail.com>
2025-05-28 10:32:29 +0300

    Refactor `Link` model to use `Mapped` annotations.
    Updated the `Link` model to include `Mapped` type hints for improved type safety and clarity.

--- app/models/link.py
+++ app/models/link.py
@@ -1,15 +1,16 @@
 from . import db
+from sqlalchemy.orm import Mapped
 
 class Link(db.Model):
     """Class representing a link in the application."""
     __tablename__ = 'links'
 
-    link_id = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(500), nullable=True)
+    link_id:Mapped[int] = db.Column(db.Integer, primary_key=True)
+    link:Mapped[str] = db.Column(db.String(500), nullable=True)
 
      # Foreign Key
-    family_id = db.Column(db.Integer, db.ForeignKey('families.family_id'), nullable=False)
+    family_id:Mapped[int] = db.Column(db.Integer, db.ForeignKey('families.family_id'), nullable=False)
 
     # Relationships
-    family = db.relationship('Family', back_populates='links')
+    family:Mapped["Family"] = db.relationship('Family', back_populates='links')
 
