35eaef130f554d7d1730ab8aacdf09967917ee03
joekariuki3 <joekariuki3@gmail.com>
2025-05-28 10:24:51 +0300

    Use SQLAlchemy `Mapped` annotations for type safety.
    Replaced raw types with `Mapped` annotations in the `Relationship` model to improve type safety and better align with modern SQLAlchemy practices.

--- app/models/relationship.py
+++ app/models/relationship.py
@@ -1,16 +1,18 @@
 from . import db
+from sqlalchemy.orm import Mapped
+from .member import Member
 
 class Relationship(db.Model):
     """Class representing relationships between family members."""
     __tablename__ = 'relationships'
 
-    relationship_id = db.Column(db.Integer, primary_key=True)
-    relationship_type = db.Column(db.String(20), nullable=False)
+    relationship_id:Mapped[int] = db.Column(db.Integer, primary_key=True)
+    relationship_type:Mapped[str] = db.Column(db.String(20), nullable=False)
 
    # Foreign Keys
-    member_id_1 = db.Column(db.Integer, db.ForeignKey('members.member_id', name='fk_member_id_1'), nullable=False)
-    member_id_2 = db.Column(db.Integer, db.ForeignKey('members.member_id', name='fk_member_id_2'), nullable=False)
+    member_id_1:Mapped[int] = db.Column(db.Integer, db.ForeignKey('members.member_id', name='fk_member_id_1'), nullable=False)
+    member_id_2:Mapped[int] = db.Column(db.Integer, db.ForeignKey('members.member_id', name='fk_member_id_2'), nullable=False)
 
     # Relationships
-    member1 = db.relationship('Member', foreign_keys=[member_id_1], back_populates='relationships1')
-    member2 = db.relationship('Member', foreign_keys=[member_id_2],  back_populates='relationships2')
+    member1:Mapped[Member] = db.relationship('Member', foreign_keys=[member_id_1], back_populates='relationships1')
+    member2:Mapped[Member] = db.relationship('Member', foreign_keys=[member_id_2],  back_populates='relationships2')
