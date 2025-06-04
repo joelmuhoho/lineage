12eb63b3607175995ff9859363308e403c9c2608
joekariuki3 <joekariuki3@gmail.com>
2025-06-04 10:57:39 +0300

    Remove `create_link` and `delete_link` routes and related `LinkService` logic from `family/routes.py` for cleanup.

--- app/family/routes.py
+++ app/family/routes.py
@@ -5,7 +5,6 @@ from .forms import CreateFamilyForm
 from app.member.forms import MemberForm
 from app.auth.services import AuthService
 from .services import FamilyService
-from app.link.services import LinkService
 from app.member.services import MemberService
 
 
@@ -78,40 +77,6 @@ def delete_family(family_id):
         return redirect(url_for('user.user_profile'))
 
     data, _ = family_service.delete_family(family_id=family_id)
-    message, category = data.get('message'), data.get('category')
-
-    flash(message, category)
-    return redirect(url_for('user.user_profile'))
-
-@bp.route('/create_link/<family_id>', methods=['POST', 'GET'])
-@login_required
-def create_link(family_id):
-    family_service = FamilyService()
-    data, status = family_service.get_family_by_id(family_id)
-    family, message, category = data.get('data'), data.get('message'), data.get('category')
-    if status != 200:
-        flash(message, category)
-        return redirect(url_for('user.user_profile'))
-    elif not family:
-        flash(message, category)
-        return redirect(url_for('user.user_profile'))
-
-    if not family_service.family_belongs_to_user(family_id=family_id, user_id=current_user.user_id):
-        flash('You are not allowed to create a link for this family', 'info')
-        return redirect(url_for('user.user_profile'))
-
-    link_service = LinkService()
-    data, status = link_service.create_link(family)
-    message, category = data.get('message'), data.get('category')
-    flash(message, category)
-    return redirect(url_for('user.user_profile'))
-
-
-@bp.route('/delete_link/<link_id>')
-@login_required
-def delete_link(link_id):
-    link_service = LinkService()
-    data, _ = link_service.delete_link(link_id=link_id)
     message, category = data.get('message'), data.get('category')
 
     flash(message, category)
