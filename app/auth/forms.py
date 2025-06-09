007977ff78211f1f2a10dea98f609d4a066014f8
joekariuki3 <joekariuki3@gmail.com>
2025-06-09 13:28:03 +0300

    Refactor `RegisterForm` to initialize `UserService` within the constructor for proper instance encapsulation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,9 +4,12 @@ from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
 from app.models import User
 from app.utils.constants import NameConstants, EmailConstants, PasswordConstants
 from app.user.services import UserService
+
 class RegisterForm(FlaskForm):
     """form for a user to create an account"""
-    user_service = UserService()
+    def __init__(self):
+        super().__init__()
+        self.user_service = UserService()
 
     name = StringField('Name', validators=[DataRequired(message=NameConstants.NameRequired)], render_kw={"placeholder": NameConstants.NamePlaceholder})
     email = EmailField('Email', validators=[DataRequired(message=EmailConstants.EmailRequired), Email(message=EmailConstants.EmailInvalid)], render_kw={"placeholder": EmailConstants.EmailPlaceholder})
@@ -17,7 +20,7 @@ class RegisterForm(FlaskForm):
     def validate_email(self, email):
         """method to check if a user with passed email exists
         if yes prompt a user to choose a different email"""
-        if user_service.check_user_exists(email=email.data):
+        if self.user_service.check_user_exists(email=email.data):
             raise ValidationError('Please use a different email address.')
 
 class LoginForm(FlaskForm):
