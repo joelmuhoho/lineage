9644ad1eb9419a58e418ca4c793ae3bc767b6dba
joekariuki3 <joekariuki3@gmail.com>
2025-06-10 15:41:15 +0300

    Refactor error Blueprint instance for improved naming consistency in route decorators to use app_errorhandler to catch 404 and 500 exceptions and return custom error pages.

--- app/error/routes.py
+++ app/error/routes.py
@@ -1,13 +1,12 @@
 from flask import render_template
-from . import bp
+from . import error_bp
 from app.extensions import db
 
-@bp.errorhandler(404)
+@error_bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@bp.errorhandler(500)
+@error_bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
-
