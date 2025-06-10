e4c7c09f31c27b8458357add87279661979b6607
joekariuki3 <joekariuki3@gmail.com>
2025-06-10 15:39:44 +0300

    Refactor `index` route in `main` Blueprint to simplify logic and remove unused `family_id` parameter

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,18 +1,6 @@
 from flask import render_template
-from app.models.family import Family
-from . import bp
-from app.utils import auth_s
+from . import main_bp
 
-@bp.route('/<family_id>')
-@bp.route('/')
-def index(family_id=''):
-    families = []
-    if family_id:
-        try:
-            data = auth_s.loads(family_id)
-            family_id = data["family_id"]
-            if family_id:
-                families = Family.query.filter_by(family_id=family_id).all()
-        except Exception as e:
-            pass
-    return render_template('index.html', title="Lineage Home", families=families)
+@main_bp.route('/')
+def index():
+    return render_template('index.html', title="Lineage Home")
