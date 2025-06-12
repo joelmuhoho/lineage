87af43adc451b8e0bb1d71aac97d19db1088d9d1
joekariuki3 <joekariuki3@gmail.com>
2025-06-12 12:15:58 +0300

    Add integration tests for event editing, including GET/POST scenarios and error handling

--- tests/integration/routes/test_event_route.py
+++ tests/integration/routes/test_event_route.py
@@ -218,7 +218,6 @@ def test_get_events(client, test_user_and_family, test_event_1):
             in the response.
     """
     user, family = test_user_and_family
-    test_event_1.family_id = family.family_id
     login_test_user_1(client, user)
 
     response = client.get(url_for("event.get_events", family_id=family.family_id), follow_redirects=True)
@@ -242,7 +241,6 @@ def test_delete_event(client, session, app, test_user_and_family, test_event_1):
         AssertionError: If the event deletion operation or related conditions fail.
     """
     user, family = test_user_and_family
-    test_event_1.family_id = family.family_id
     login_test_user_1(client, user)
 
     # before deletion assert event exists
@@ -285,7 +283,6 @@ def test_delete_event_error(client, session, app, test_user_and_family, test_eve
             event existence in the database fails.
     """
     user, family = test_user_and_family
-    test_event_1.family_id = family.family_id
     login_test_user_1(client, user)
 
     # before deletion assert event exists
@@ -310,5 +307,158 @@ def test_delete_event_error(client, session, app, test_user_and_family, test_eve
     # assert event still exists after an error in deletion
     with app.app_context():
         assert session.query(Event).count() == 1
+
+    monkeypatch.undo()
+
+def test_edit_event_get_non_existing_event(client, test_user_and_family):
+    """
+    Tests the behavior of the `edit_event` route when attempting to retrieve a non-existing event.
+
+    This test ensures that when a user tries to access the event editing page for an event that does not exist,
+    the application handles the situation gracefully by returning a response with status code 200 and
+    providing a message indicating that the event was not found.
+
+    Parameters:
+    client: The test client used to simulate HTTP requests.
+    test_user_and_family: A fixture that provides a test user and their associated family structure.
+
+    Raises:
+    AssertionError: If the response status code is not 200 or the expected "Event not found" message
+                    is not present in the response data.
+    """
+    user, _ = test_user_and_family
+    login_test_user_1(client, user)
+
+    response = client.get(url_for("event.edit_event", event_id=999999), follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b"Event not found" in response.data
+
+def test_edit_event_get_event_not_owned_by_user(client, test_user_and_family, test_event_2):
+    """
+    Tests that a user cannot access the edit event page for an event not owned by them. It ensures that a proper
+    unauthorized message is displayed when attempting to edit another user's event.
+
+    Parameters:
+    client: FlaskClient
+        The test client used to make requests to the application.
+    test_user_and_family: tuple
+        A tuple containing the test user object and associated family information.
+    test_event_2: Event
+        An event instance that is not owned by the test user.
+
+    Raises:
+    AssertionError
+        If the response status code is not 200, or the unauthorized message is not present in the response data.
+    """
+    user, _ = test_user_and_family
+    login_test_user_1(client, user)
+    response = client.get(url_for("event.edit_event", event_id=test_event_2.event_id), follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b"You are not authorized to edit this event" in response.data
+
+def test_edit_event_get(client, test_user_and_family, test_event_1):
+    """
+    Tests the GET method of the edit_event view.
+
+    This function logs in a test user, sends a GET request to the edit_event
+    endpoint with a specified event ID, and verifies the HTTP response status
+    code and the presence of specific content in the response data.
+
+    Args:
+        client: Flask test client for sending requests to the application.
+        test_user_and_family: Tuple containing a test user object and a test
+            family object.
+        test_event_1: Test event object whose details are to be fetched.
+
+    Raises:
+        AssertionError: If the response code or content does not match
+            expected values.
+    """
+    user, family = test_user_and_family
+    login_test_user_1(client, user)
+
+    response = client.get(url_for("event.edit_event", event_id=test_event_1.event_id), follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b"Update Event details" in response.data
+    assert b"event_1" in response.data
+
+def test_edit_event_post(client, test_user_and_family, test_event_1):
+    """
+    Tests the functionality of editing an event using a POST request. This test checks if an authenticated user can update an event's details successfully and if the expected
+    changes are reflected in the response. It includes verifying success messages, updated event details, and the HTTP response status.
+
+    Parameters:
+        client: FlaskClient
+            The test client used to simulate HTTP requests.
+        test_user_and_family: Tuple[User, Family]
+            A tuple containing a test user and their associated family.
+        test_event_1: Event
+            The test event that will be updated.
+
+    Raises:
+        AssertionError: If the response status code, success message, or updated event details
+            do not match the expected results.
+    """
+    user, _ = test_user_and_family
+    login_test_user_1(client, user)
+    event_data = {
+        "name": "Updated Event",
+        "date": datetime(2025, 12, 25),
+        "family": test_event_1.family_id,
+        "description": "Updated Description",
+        "location": "Updated Location"
+    }
+    response = client.post(url_for("event.edit_event", event_id=test_event_1.event_id), data=event_data, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Event updated successfully" in response.data
+    assert b"Updated Event" in response.data
+
+def test_edit_event_post_error(client, test_user_and_family, test_event_1, monkeypatch):
+    """
+    Tests the behavior of the edit_event POST route when an error occurs during
+    event update. This function simulates a failure in the event update service,
+    intercepts the update method of EventService to return an error response,
+    and verifies the client's response and behavior when attempting to update an
+    event under these conditions.
+
+    Args:
+        client: Flask test client used to interact with the application during
+            tests.
+        test_user_and_family: Tuple consisting of test user information and family
+            associated with the user.
+        test_event_1: An event object representing a test event being edited.
+        monkeypatch: Pytest-provided fixture for dynamically modifying objects or
+            methods during tests.
+
+    Raises:
+        AssertionError: Raised if assertions within the test fail, indicating
+            incorrect behavior or unexpected results.
+    """
+    user, _ = test_user_and_family
+    login_test_user_1(client, user)
+
+    def mock_update_event(*args, **kwargs):
+        return service_response(
+            HTTPStatus.INTERNAL_SERVER_ERROR,
+            "Error updating event",
+            "danger",
+            None
+        )
+    monkeypatch.setattr(EventService, "update_event", mock_update_event)
+
+    event_data = {
+        "name": "Updated Event",
+        "date": datetime(2025, 12, 25),
+        "family": test_event_1.family_id,
+        "description": "Updated Description",
+        "location": "Updated Location"
+    }
+    response = client.post(url_for("event.edit_event", event_id=test_event_1.event_id), data=event_data, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Error updating event" in response.data
+    assert b"event_1" in response.data # the event name should not change
 
     monkeypatch.undo()
