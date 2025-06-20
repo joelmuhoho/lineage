85cb004a5328fe6675855b0d0575422e3bc13ca5
joekariuki3 <joekariuki3@gmail.com>
2025-06-20 14:06:27 +0300

    Refactor `create_link` route: switch to POST-only method, retrieve `family_id` from form, validate input, and streamline service calls

--- app/link/routes.py
+++ app/link/routes.py
@@ -1,24 +1,24 @@
 from . import link_bp
-from flask import flash, redirect, url_for
+from flask import flash, redirect, url_for, request
 from app.family.services import FamilyService
 from .services import LinkService
 from flask_login import login_required, current_user
 
-@link_bp.route('/create_link/<family_id>', methods=['POST', 'GET'])
+@link_bp.route('/create_link', methods=['POST'])
 @login_required
-def create_link(family_id):
+def create_link():
+    family_id = request.form.get('family_id')
+    if not family_id:
+        flash('Family id is required', 'info')
+        return redirect(url_for('user.user_profile'))
+
+    family_id = int(family_id)
+
     family_service = FamilyService()
-    data, status = family_service.get_family_by_id(family_id)
+    data, status = family_service.get_family_by_id(family_id=family_id, user_id=current_user.user_id)
     family, message, category = data.get('data'), data.get('message'), data.get('category')
     if status != 200:
         flash(message, category)
-        return redirect(url_for('user.user_profile'))
-    elif not family:
-        flash(message, category)
-        return redirect(url_for('user.user_profile'))
-
-    if not family_service.family_belongs_to_user(family_id=family_id, user_id=current_user.user_id):
-        flash('You are not allowed to create a link for this family', 'info')
         return redirect(url_for('user.user_profile'))
 
     link_service = LinkService()
